b06a1950596410b1c31a227ea2ee6057353d876d
徐家琪 <xv_jiaqi@163.com>
2025-03-02 15:52:25 +0800

    feat: 更新百度 cookies 并调整验证码求解器重试次数
    - 更新 baidu.com.txt 和 baidu.com.txt.bak 中的 cookies
    - 将 baidu_rotate_captcha_solver.py 中的最大重试次数从 20 增加到 30

--- src/verification/baidu/baidu_rotate_captcha_solver.py
+++ src/verification/baidu/baidu_rotate_captcha_solver.py
@@ -6,7 +6,7 @@ from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def solve_rotation_captcha(driver, max_retries=20):
+def solve_rotation_captcha(driver, max_retries=30):
     """
     解决百度旋转验证码，如果失败会自动重试
     
